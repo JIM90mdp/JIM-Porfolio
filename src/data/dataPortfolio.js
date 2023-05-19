74ffcbe076388c7786262ae532b67af2882ad2df
JIM90mdp <juanignaciomascarenhas@gmail.com>
2023-05-19 18:35:21 -0300

    Refactor/dataPortfolio

---
+++ src/data/dataPortfolio.js
@@ -0,0 +1,194 @@
+export const greetings = {
+  title: "Welcome!",
+  name: "Juan I. Mascarenhas",
+  subTitle: "Frontend Web Developer",
+  desc: [
+    "I’m embracing a new obsession, learning Front-End Web Development.",
+    "I have experience building Applications with JavaScript, React, Node, Express, Redux, Next and some other cool libraries and frameworks.",
+  ],
+};
+
+export const openSource = {
+  githubUserName: "JIM90mdp",
+};
+
+export const links = {
+  url: "https://mascarenhas-dev.vercel.app/",
+  instagram: "https://www.instagram.com/nachomascarenhas/",
+  github: "https://github.com/JIM90mdp",
+  linkedin: "https://www.linkedin.com/in/mascarenhas-developer/",
+};
+
+export const skillsSection = {
+  data: [
+    {
+      title: "Front-End Development",
+      skills: [
+        " Design and development of applications specializing in: JavaScript, React, Nextjs and Tailwind",
+        " Proficient in Angular, TypeScript, MUI and Bootstrap",
+        "I focus on developing modular and scalable designs and applications that meet the client's needs using agile methodologies to reach the final product.",
+      ],
+      softwareSkills: [
+        {
+          skillName: "javascript",
+          fontAwesomeClassname: "skill-icons:javascript",
+        },
+        {
+          skillName: "typescript",
+          fontAwesomeClassname: "logos:typescript-icon-round",
+        },
+        {
+          skillName: "python",
+          fontAwesomeClassname: "logos:python",
+        },
+        {
+          skillName: "nodejs",
+          fontAwesomeClassname: "logos:nodejs-icon",
+        },
+        {
+          skillName: "reactjs",
+          fontAwesomeClassname: "vscode-icons:file-type-reactjs",
+        },
+        {
+          skillName: "nextJS",
+          fontAwesomeClassname: "logos:nextjs-icon",
+        },
+
+        {
+          skillName: "tailwindcss",
+          fontAwesomeClassname: "devicon:tailwindcss",
+        },
+        {
+          skillName: "figma",
+          fontAwesomeClassname: "logos:figma",
+        },
+      ],
+      softSkills: [
+        "Communication",
+        "Proble solving",
+        "Teamwork",
+        "Organizational",
+        "Critical thinking",
+        "Creativity",
+        "Patience",
+      ],
+    },
+  ],
+};
+
+export const skillStack = [
+  {
+    Stack: "Programming",
+  },
+  {
+    Stack: "Frontend/Design",
+  },
+];
+
+export const educationInfo = [
+  {
+    schoolName: "Henry Bootcamp",
+    subHeader: "Full Stack Web Developer",
+    duration: "August 2022 - March 2023",
+    desc: "Programming Bootcamp to learn Full Stack Development",
+    bullets: [
+      "  Object-Oriented Programming: JavaScript",
+      "  Web Application Development: HTML, CSS, JavaScript, React, Redux, fetch to APIs",
+      "  Database Programming: Table Design, Sequelize, PostgreSQL, E/R diagrams",
+      "  Completed 1000+ hours of JavaScript, React, Sequelize, and PostreSQL training",
+      "  Completed 100+ hours of pair programming on group projects",
+    ],
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    name: "jim's countries",
+    link: "https://jim-countries-vbeb.vercel.app/",
+    imgTitle: "JIM Countries",
+    desc: [
+      "Website created as an individual project of SoyHenry's Full-Stack Web Development bootcamp.",
+      "The purpose of J.I.M+'s Countries website is to provide information about all countries in the world and create activities, making the tourist trip planning easier.",
+      "The information is provided by RESTful Application Program Interface restcountries.com",
+    ],
+    github: ["https://github.com/JIM90mdp/JIM-Countries"],
+    tags: [
+      "JavaScript",
+      "Node",
+      "Express",
+      "React",
+      "Redux",
+      "PostgreSQL",
+      "Sequelize",
+      "CSS",
+      "SVC",
+    ],
+  },
+  {
+    id: 2,
+    name: "groove tickets",
+    link: "https://groove-tickets.vercel.app/",
+    img: "Groove Tickets",
+    text: [
+      "Website created as a group project of SoyHenry's Full-Stack Web Development bootcamp.",
+      "The purpose of the Groove Tickets website is to facilitate the acquisition of tickets for concerts and other live music shows and to provide detailed information about the events.",
+    ],
+    github: [
+      "https://github.com/JIM90mdp/Groove-TIckets-Back",
+      "https://github.com/JIM90mdp/Groove-Tickets-Front",
+    ],
+    tags: [
+      "JavaScript",
+      "Node",
+      "Express",
+      "React",
+      "Redux",
+      "React Admin",
+      "PostgreSQL",
+      "Sequelize",
+      "MUI",
+      "SVC",
+    ],
+  },
+  {
+    id: 3,
+    name: "jim's porfolio",
+    link: "https://mascarenhas-dev.vercel.app/",
+    img: "JIM Porfolio",
+    text: [
+      "Design and development of a portfolio to provide personal and professional information.",
+    ],
+    github: ["https://github.com/JIM90mdp/JIM-Porfolio"],
+    tags: ["JavaScript", "React", "Next", "Tailwind", "Framer Motion", "VSC"],
+  },
+  {
+    id: 4,
+    name: "mmc - servicios jurídicos",
+    link: "",
+    img: "",
+    text: [
+      "API for a lawyer client, allowing users to learn about the legal services provided, submit free legal inquiries, and access information related to legal services.",
+    ],
+    github: [],
+    tags: ["JavaScript", "React", "Next", "Tailwind", "Framer Motion", "VSC"],
+  },
+];
+
+export const seoData = {
+  title: "JIM's Portfolio",
+  description: "Front-End Web Developer displaying my portfolio and professional background.",
+  url: "https://mascarenhas-dev.vercel.app/",
+  keywords: [
+    "Juan Ignacio Mascarenhas",
+    "Portfolio",
+    "Front-End Web Developer",
+    "Front-End Developer",
+    "Front-End",
+    "Developer",
+    "Argentina",
+    "Buenos Aires",
+    "Mar del Plata",
+    "Open to work", 
+  ],
+};
